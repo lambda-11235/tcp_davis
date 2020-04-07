0070bb0af0934bd5fb8a69eed883197cf6dfaa25
Taran Lynn <taranlynn0@gmail.com>
2020-04-06 22:30:18 -0700

    Implement test runner

---
+++ tests/run.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -e
+
+
+if [ $# -lt 2 ]
+then
+    echo "Usage: $0 TOOL OUTDIR DEST"
+    exit 1
+fi
+
+TOOL=$1
+OUTDIR=`realpath $2`
+DEST=$3
+
+if [ $TOOL != "iperf3" ] && [ $TOOL != "pschedular" ]
+then
+    echo "TOOL must be one of [iperf3, pschedular]"
+    exit 1
+fi
+
+
+CURDIR=`pwd`
+CC_ALGO=`sysctl -n net.ipv4.tcp_congestion_control`
+
+
+for flows in 1 2 5
+do
+    DIR=$OUTDIR/$flows-flows
+
+    if [ ! -e $DIR ]
+    then
+        mkdir $DIR
+    fi
+    
+    cd $DIR
+
+    for cc in dumb reno vegas bbr
+    do
+        sysctl net.ipv4.tcp_congestion_control=$cc
+
+        case $TOOL in
+            "iperf3" ) iperf3 -t 60 -i 0.1 -c $DEST -P $flows --json > $DIR/$cc.json;;
+            "pschedular" ) echo "pschedular unimplemented"; exit 1;;
+            * ) echo "Unrecognized tool"; exit 1;;
+        esac
+    done
+done
