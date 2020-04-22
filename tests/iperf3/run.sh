61418e407bcb03d015c85bb88a48693118164115
Taran Lynn <taranlynn0@gmail.com>
2020-04-22 12:18:27 -0700

    Add Flent batch script

---
+++ tests/iperf3/run.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+set -e
+
+
+if [ $# -lt 2 ]
+then
+    echo "Usage: $0 TOOL OUTDIR DEST"
+    exit 1
+fi
+
+TOOL=$1
+OUTDIR=`realpath $2`
+DEST=$3
+
+if [ $TOOL != "iperf3" ] && [ $TOOL != "pscheduler" ]
+then
+    echo "TOOL must be one of [iperf3, pscheduler]"
+    exit 1
+fi
+
+
+CURDIR=`pwd`
+CC_ALGO=`sysctl -n net.ipv4.tcp_congestion_control`
+
+
+for flows in 1 2 5
+do
+    DIR=$OUTDIR/$flows-flows
+
+    if [ ! -e $DIR ]
+    then
+        mkdir $DIR
+    fi
+
+    cd $DIR
+
+    for cc in davis bbr reno vegas
+    do
+        sysctl net.ipv4.tcp_congestion_control=$cc
+
+        case $TOOL in
+            "iperf3" ) iperf3 -t 60 -c $DEST -P $flows --json > $DIR/$cc.json;;
+            "pscheduler" ) pscheduler task --tool iperf3 --format=json --quiet throughput -t PT1M -P $flows --dest $DEST | head -n1 > $DIR/$cc.json;;
+            * ) echo "Unrecognized tool"; exit 1;;
+        esac
+    done
+done
+
+sysctl net.ipv4.tcp_congestion_control=$CC_ALGO
--- tests/netem_setup.sh
+++ tests/netem_setup.sh
@@ -1,26 +1,39 @@
 #!/bin/sh
 
-if [ $# -lt 2 ]
+if [ $# -lt 3 ]
 then
-    echo "Usage: $0 DEV RTT"
+    echo "Usage: $0 DEV RATE RTT"
     exit 1
 fi
 
 DEV=$1
-RTT=$2
+RATE=$2 # Bytes/sec
+RTT=$3  # ms
 
 run() {
     echo $@
     $@
 }
 
-sysctl net.core.rmem_default=2147483647
-sysctl net.core.rmem_max=2147483647
 
-sysctl net.core.wmem_default=2147483647
-sysctl net.core.wmem_max=2147483647
+MEM=`echo "2^31 - 1" | bc`
+echo $MEM
+BUFSIZE=`echo "$RATE*$RTT/1000" | bc`
 
-sysctl net.ipv4.tcp_rmem="2147483647 2147483647 2147483647"
-sysctl net.ipv4.tcp_wmem="2147483647 2147483647 2147483647"
+if [ $BUFSIZE -lt $MEM ]
+then
+    MEM=$BUFSIZE
+fi
 
-run tc qdisc replace dev $DEV root netem delay $RTT limit 1G
+echo $MEM
+
+sysctl net.core.rmem_default=$MEM
+sysctl net.core.rmem_max=$MEM
+
+sysctl net.core.wmem_default=$MEM
+sysctl net.core.wmem_max=$MEM
+
+sysctl net.ipv4.tcp_rmem="$MEM $MEM $MEM"
+sysctl net.ipv4.tcp_wmem="$MEM $MEM $MEM"
+
+run tc qdisc replace dev $DEV root netem delay ${RTT}ms limit 1G
