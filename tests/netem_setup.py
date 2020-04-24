22fcf42a1f6021da08953fa60517ac384865f871
Taran Lynn <taranlynn0@gmail.com>
2020-04-24 00:17:34 -0700

    Make rate param always have a value, make TBF limit be BDP

--- tests/netem_setup.py
+++ tests/netem_setup.py
@@ -16,26 +16,24 @@ def fraction(s):
 parser = argparse.ArgumentParser(description="Setup server (NOT client) for NetEm testing.")
 parser.add_argument('dev', type=str,
                     help="Network device to attach NetEm QDisc to.")
-parser.add_argument('--rate', type=str,
-                    help="Rate to limit traffic to in bits/s (default: no limit).")
+parser.add_argument('--rate', type=str, default="10gbits",
+                    help="Rate to limit traffic to in bits/s (default: %(default)s).")
 parser.add_argument('--rtt', type=str, default="30ms",
                     help="Emulated RTT (default: %(default)s).")
-parser.add_argument('--limit', type=str,
-                    help="NetEm buffer length limit.")
+parser.add_argument('--max-buf-size', type=int, default=int(2**31 - 1),
+                    help="Maximum size of buffers in bytes (default: %(default)s).")
+parser.add_argument('--min-mtu', type=int, default=576,
+                    help="Minimum MTU for server (default: %(default)s).")
 parser.add_argument('--burst-frac', type=fraction, default=0.01,
                     help="Burstiness of TBF as fraction (in range [0, 1]) of buffer size (default: %(default)s).")
 args = parser.parse_args()
 
-
-maxBufSize = int(2**31 - 1)
-minMTU = 576
 
 floatRegex = '([0-9]+(\.[0-9]+)?)'
 siRegex = "(T|G|M|K|t|g|m|k)"
 
 rateRegex = '{}{}?(bits|bytes)'.format(floatRegex, siRegex)
 rttRegex = '{}(s|ms|us)?'.format(floatRegex)
-limitRegex = '{}{}?'.format(floatRegex, siRegex)
 
 siScales = {'T': 1000**4, 'G': 1000**3, 'M': 1000**2, 'K': 1000,
               't': 2**40, 'g': 2**30, 'm': 2**20, 'k': 2**10}
@@ -44,8 +42,11 @@ rttScales = {'s': 1, 'ms': 1.0e-3, 'us': 1.0e-6}
 
 
 def run(command, ignoreFailure=False):
-    comArgs = command.split()
-    
+    if isinstance(command, str):
+        comArgs = command.split()
+    else:
+        comArgs = command
+
     print(str.join(' ', comArgs))
     p = Popen(comArgs)
 
@@ -61,30 +62,23 @@ def getSysctl(attr):
         return (p.stdout.read().decode('utf-8').splitlines()[0], p)
 
 def setSysctl(attr, value):
-    p = Popen(["sysctl", "-w", "{}={}".format(attr, value)])
-
-    if p.wait() != 0:
-        raise RuntimeError("sysctl exited with {}".format(p.returncode))
-    else:
-        return p
+    run(["sysctl", "-qw", "{}={}".format(attr, value)])
 
 
 ### Parse Rate ###
-rate = None
-if args.rate is not None:
-    m = re.compile(rateRegex).fullmatch(args.rate)
+m = re.compile(rateRegex).fullmatch(args.rate)
 
-    if m is None:
-        raise RuntimeError("Rate must conform to \"{}\".".format(rateRegex))
-    
-    (num, _, scale, meas) = m.groups()
-    rate = float(num)
+if m is None:
+    raise RuntimeError("Rate must conform to \"{}\".".format(rateRegex))
 
-    if scale is not None:
-        rate *= siScales[scale]
+(num, _, scale, meas) = m.groups()
+rate = float(num)
 
-    if meas != 'bytes':
-        rate /= 8
+if scale is not None:
+    rate *= siScales[scale]
+
+if meas != 'bytes':
+    rate /= 8
 
 ### Parse RTT ###
 m = re.compile(rttRegex).fullmatch(args.rtt)
@@ -98,38 +92,23 @@ rtt = float(num)
 if scale is not None:
     rtt *= rttScales[scale]
 
-### Compute MSS ###
+### Compute BDP ###
 if rate is not None:
-    bdp = rate*rtt
-    
-    bufsize = min(maxBufSize, math.ceil(2*bdp))
+    bdp = min(args.max_buf_size, math.ceil(rate*rtt))
+    bdp2 = min(args.max_buf_size, math.ceil(2*rate*rtt))
 else:
-    bufsize = maxBufSize
-
-### Parse Limit ###
-if args.limit is not None:
-    m = re.compile(limitRegex).fullmatch(args.limit)
-
-    if m is None:
-        raise RuntimeError("Limit must conform to \"{}\".".format(limitRegex))
-
-    (num, _, scale) = m.groups()
-    limit = float(num)
-
-    if scale is not None:
-        limit *= siScales[scale]
-else:
-    limit = bufsize
+    bdp = args.max_buf_size
+    bdp2 = args.max_buf_size
 
 
-setSysctl("net.core.rmem_default", bufsize)
-setSysctl("net.core.rmem_max", bufsize)
+setSysctl("net.core.rmem_default", bdp2)
+setSysctl("net.core.rmem_max", bdp2)
 
-setSysctl("net.core.wmem_default", bufsize)
-setSysctl("net.core.wmem_max", bufsize)
+setSysctl("net.core.wmem_default", bdp2)
+setSysctl("net.core.wmem_max", bdp2)
 
-setSysctl("net.ipv4.tcp_rmem", "{0} {0} {0}".format(bufsize))
-setSysctl("net.ipv4.tcp_wmem", "{0} {0} {0}".format(bufsize))
+setSysctl("net.ipv4.tcp_rmem", "{0} {0} {0}".format(bdp2))
+setSysctl("net.ipv4.tcp_wmem", "{0} {0} {0}".format(bdp2))
 
 run("tc qdisc del dev ifb0 root", ignoreFailure=True)
 run("tc qdisc del dev {} ingress".format(args.dev), ignoreFailure=True)
@@ -142,11 +121,11 @@ run("tc filter add dev {} parent ffff: ".format(args.dev) +
 
 if rate is not None:
     run("tc qdisc add dev ifb0 root handle 1: tbf rate {}bit burst {} limit {}".format(
-        math.ceil(8*rate), math.ceil(bufsize*args.burst_frac), limit))
+        math.ceil(8*rate), math.ceil(bdp*args.burst_frac), bdp))
 
     base = "parent 1:"
 else:
     base = "root"
 
 run("tc qdisc add dev ifb0 {} netem delay {} limit {}".format(
-    base, str(round(rtt*1e6)), str(math.ceil(limit/minMTU))))
+    base, round(rtt*1e6), math.ceil(bdp2/args.min_mtu)))
